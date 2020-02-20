c7d1740510179cb3aaf1cdc0768e2fa682e0c574
spensation <spencevans@gmail.com>
2020-02-20 11:49:20 -0500

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,10 @@ require 'pry'
 class Dog
 	attr_accessor :id, :name, :breed
 
-	def initialize(dog)
-		@name = dog[:name]
-		@breed = dog[:breed]
+	def initialize(id: nil, name:, breed:)
+		@id = id
+		@name = name
+		@breed = breed
 	end
 
 	def self.create_table
@@ -26,14 +27,19 @@ class Dog
 	end
 
 	def save
-	  sql = <<-SQL 
-	    INSERT INTO dogs (name, breed)
-	    VALUES (?, ?)
-	    SQL
+	  if self.id
+	  	self.update
+	  else
+		sql = <<-SQL 
+		  INSERT INTO dogs (name, breed)
+		  VALUES (?, ?)
+		  SQL
 
-	  DB[:conn].execute(sql, self.name, self.breed)
+		DB[:conn].execute(sql, self.name, self.breed)
 
-	  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+		@id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      end
+      self
 	end
 
 	def self.create(dog)
@@ -73,6 +79,31 @@ class Dog
 	  DB[:conn].execute(sql,name).map do |row|
 	    self.new_from_db(row)
 	  end.first
+	end
+
+	def self.find_or_create_by(name:, breed:)
+	  sql = <<-SQL
+	  	SELECT *
+	  	FROM dogs
+	  	WHERE name = ?
+	  	AND breed = ?
+	  	LIMIT 1
+	  SQL
+
+	  dog = DB[:conn].execute(sql, name, breed)
+
+	  if !dog.empty?
+	  	dog_data = dog[0]
+	  	dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+	  else
+	  	dog = self.create(name: name, breed: breed)
+	  end
+	  dog
+	end
+
+	def update 
+	  sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+	  DB[:conn].execute(sql, self.name, self.breed, self.id)
 	end
 
 end
